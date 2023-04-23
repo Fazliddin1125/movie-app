4c7b4cbf2bcb50edd147049455fcc338525a5965
Fazliddin <fazliddinbahromov9717@gmail.com>
2023-04-23 19:52:26 +0500

    p tag

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -10,7 +10,7 @@ const Modal = () => {
   return (
     <MuiModal open={modal} onClose={handleClose}>
         <>
-         {currentMovie.title}
+         {currentMovie.title} <p>Salom</p>
          <button onClick={()=>setModal(false)} className="modalButton absolute w-9 h-9 right-5 top-5 !z-40 border-none bg-[#181818]" > <FaTimes /></button>
         </>
     </MuiModal>
